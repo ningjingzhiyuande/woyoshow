92a2e5c748af5c5fae447070562ab963dfb60c55
sgd <sgd@sgd-laptop.(none)>
2011-01-14 15:03:15 +0800

    woyoshow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woyoshow::Application.config.secret_token = 'b3d408f5a1ad826ea021fa8c5abd497a9b2c934ef36966de3a6c9b0ed5405e4c442321956f96d72e2c907c9bcae0ee4f2ff70d75a18bb54d9713f2612ec961b1'
